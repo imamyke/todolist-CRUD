88c20e860b298fc3782065f0f5906aae8a80c314
Amy Ke <xiahamy@gmail.com>
2023-02-08 23:48:16 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -34,10 +34,16 @@ const usePassport = require('./config/passport')
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+// 引用 connect-flash
+const flash = require('connect-flash')
+app.use(flash())  // 掛載套件
+
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
